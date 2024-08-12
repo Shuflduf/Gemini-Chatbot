bd51f7cf8fd88b48050b8c513bce2902b836f8f9
Shuflduf <shuflduf@gmail.com>
2024-08-12 13:09:30 -0600

    succesful http

--- main.gd
+++ main.gd
@@ -14,24 +14,26 @@ func ask(prompt: String) -> String :
 			"https://generativelanguage.googleapis.com/v1beta/models/" \
 			+ model + ":generateContent?key=" + api_key
 
-	#var body = {
-		#"contents": [{
-			#"role": "user",
-#
-			#"parts": [{
-				#"text": prompt,
-			#}],
-		#}],
-	#}
+	var body = {
+		"contents": [{
+			"role": "user",
 
-	var headers = PackedStringArray(["contents: [{'role': 'user', 'parts': [{'text': " + prompt + "}]}]"])
+			"parts": [{
+				"text": prompt,
+			}],
+		}],
+	}
 
-	request.request(url, headers)
+	#var headers = PackedStringArray(["contents: [{'role': 'user', 'parts': [{'text': " + prompt + "}]}]"])
+
+	request.request(url, [{"Content-Type": "application/json"}], 0, str(body))
 	request.request_completed.connect(_on_http_request_request_completed)
 
 	return ""
 
 
-func _on_http_request_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
-	print("SMTH")
-	print(response_code)
+func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	print("Result: ", result)
+	print("Code: ", response_code)
+	print("Headers: ", headers)
+	print("Body: ", body)
