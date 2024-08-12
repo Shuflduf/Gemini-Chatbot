9f90bf6a3a93c6549b91d91a13b6118b718061f7
Shuflduf <shuflduf@gmail.com>
2024-08-12 12:58:37 -0600

    sending a req

--- main.gd
+++ main.gd
@@ -1,22 +1,37 @@
 extends Control
 
+@onready var request: HTTPRequest = $HTTPRequest
 
-var model = ""
-var api_key = ""
+var model = "gemini-pro"
+@export var api_key = ""
+
+func _ready() -> void:
+	ask("Hello")
+
 
 func ask(prompt: String) -> String :
 	var url = \
 			"https://generativelanguage.googleapis.com/v1beta/models/" \
 			+ model + ":generateContent?key=" + api_key
 
-	var body = {
-		"contents": [{
-			"role": "user",
+	#var body = {
+		#"contents": [{
+			#"role": "user",
+#
+			#"parts": [{
+				#"text": prompt,
+			#}],
+		#}],
+	#}
 
-			"parts": [{
-				"text": prompt,
-			}],
-		}],
-	}
+	var headers = PackedStringArray(["contents: [{'role': 'user', 'parts': [{'text': " + prompt + "}]}]"])
+
+	request.request(url, headers)
+	request.request_completed.connect(_on_http_request_request_completed)
 
 	return ""
+
+
+func _on_http_request_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+	print("SMTH")
+	print(response_code)
