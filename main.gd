cd96f2b47f5e2df30c3d61971b555f87a745a91b
Shuflduf <shuflduf@gmail.com>
2024-08-12 13:37:51 -0600

    safety settings and change model to 1.5 flash

--- main.gd
+++ main.gd
@@ -1,9 +1,8 @@
 extends Control
 
 @onready var request: HTTPRequest = $HTTPRequest
-
 @export var secrets: Secrets
-var model = "gemini-pro"
+var model = "gemini-1.5-flash"
 
 
 func _ready() -> void:
@@ -22,11 +21,33 @@ func ask(prompt: String) -> String :
 			"parts": [{
 				"text": prompt,
 			}],
+
 		}],
-	}
+		  "safety_settings": [
+			{
+			  "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+			  "threshold": "BLOCK_NONE"
+			},
+
+			{
+			  "category": "HARM_CATEGORY_HATE_SPEECH",
+			  "threshold": "BLOCK_NONE"
+			},
+
+			{
+			  "category": "HARM_CATEGORY_HARASSMENT",
+			  "threshold": "BLOCK_NONE"
+			},
+
+			{
+			  "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+			  "threshold": "BLOCK_NONE"
+			}
+		  ]
+		}
 
 	#var headers = PackedStringArray(["contents: [{'role': 'user', 'parts': [{'text': " + prompt + "}]}]"])
-
+	#print(str(body))
 	request.request(url, [{"Content-Type": "application/json"}], HTTPClient.METHOD_GET, str(body))
 	request.request_completed.connect(_on_http_request_request_completed)
 
@@ -34,10 +55,11 @@ func ask(prompt: String) -> String :
 
 
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	print("Result: ", result)
-	print("Code: ", response_code)
-	print("Headers: ", headers)
-	#var data = body.decode_var(0)
-	#print("Data: ", data)
-	print("Body: ", body)
-	FileAccess.open("save.html", FileAccess.WRITE).store_buffer(body)
+	if true:
+		print("Result: ", result)
+		print("Code: ", response_code)
+		print("Headers: ", headers)
+		#var data = body.decode_var(0)
+		#print("Data: ", data)
+		print("Body: ", body)
+		FileAccess.open("save.html", FileAccess.WRITE).store_buffer(body)
