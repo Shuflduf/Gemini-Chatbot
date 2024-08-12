0777f0e3eb72ece5c304dee1d6ecd97ab3d5b283
Shuflduf <shuflduf@gmail.com>
2024-08-12 17:33:34 -0600

    italic formatter

--- MDtoBB.gd
+++ MDtoBB.gd
@@ -6,19 +6,35 @@ func md_to_bb(markdown_text: String) -> String:
 	var bb_string = markdown_text
 
 	bb_string = bold(bb_string)
+	bb_string = italic(bb_string)
 
 	return bb_string
 
 func bold(markdown_bold_text: String) -> String:
 
-	var bb_bold_string = markdown_bold_text
+	var bb_string = markdown_bold_text
 
-	for bold in bb_bold_string.count("**") / 2:
-		var start = bb_bold_string.findn("**")
-		var end = bb_bold_string.findn("**", start + 1)
-		bb_bold_string = bb_bold_string.erase(start, 2)
-		bb_bold_string = bb_bold_string.insert(start, "[b]")
-		bb_bold_string = bb_bold_string.erase(end + 1, 2)
-		bb_bold_string = bb_bold_string.insert(end + 1, "[/b]")
-		print(start, " : ", end)
-	return bb_bold_string
+
+	for i in ceil(bb_string.count("**") / 2.0):
+		var start = bb_string.findn("**")
+		var end = bb_string.findn("**", start + 1)
+		bb_string = bb_string.erase(start, 2)
+		bb_string = bb_string.insert(start, "[b]")
+		bb_string = bb_string.erase(end + 1, 2)
+		bb_string = bb_string.insert(end + 1, "[/b]")
+
+	return bb_string
+
+func italic(markdown_italic_text: String) -> String:
+
+	var bb_string = markdown_italic_text
+
+
+	for i in ceil(bb_string.count("*") / 2.0):
+		var start = bb_string.findn("*")
+		var end = bb_string.findn("*", start + 1)
+		bb_string = bb_string.erase(start, 1)
+		bb_string = bb_string.insert(start, "[i]")
+		bb_string = bb_string.erase(end + 2, 1)
+		bb_string = bb_string.insert(end + 2, "[/i]")
+	return bb_string
