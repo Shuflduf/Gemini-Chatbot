fd7ce24bb7697019f92dfa9c7de784d77fd31c60
Shuflduf <shuflduf@gmail.com>
2024-08-12 18:21:23 -0600

    single line code

--- MDtoBB.gd
+++ MDtoBB.gd
@@ -5,9 +5,28 @@ func md_to_bb(markdown_text: String) -> String:
 
 	var bb_string = markdown_text
 
+	#var code_text
+
+	bb_string = code(bb_string)
 	bb_string = bold(bb_string)
 	bb_string = italic(bb_string)
 	bb_string = heading(bb_string)
+
+	return bb_string
+
+func code(markdown_code_text: String) -> String:
+
+	var bb_string = markdown_code_text
+
+	for i in ceil(bb_string.count("`") / 2.0):
+
+		var start = bb_string.findn("`")
+		bb_string = bb_string.erase(start, 1)
+		bb_string = bb_string.insert(start, "[code]")
+
+		var end = bb_string.findn("`", start + 1)
+		bb_string = bb_string.erase(end, 1)
+		bb_string = bb_string.insert(end, "[/code]")
 
 	return bb_string
 
@@ -44,7 +63,7 @@ func heading(markdown_heading_text: String) -> String:
 
 	if bb_string.begins_with("# "):
 		bb_string = bb_string.erase(0, 2)
-		bb_string = bb_string.insert(0, "[font_size=40]")
+		bb_string = bb_string.insert(0, "[font_size=30]")
 		bb_string = bb_string.insert(bb_string.find("\n", 1), "[/font_size]")
 
 	return bb_string
